e7cc0150b9335c592b883e095fcf42c233815c15
Ole Martin Hansen Kopperud <olemha@hiof.no>
2026-02-20 13:56:12 +0100

    Forelesning Item, Item, Item
    Item, Item, Item, Item

---
+++ Routing/src/components/Category.jsx
@@ -0,0 +1,40 @@
+// Importerer useParams-hooken fra react-router-dom
+// useParams brukes for å hente URL-parametere (f.eks :slug)
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useOutletContext, useParams } from 'react-router-dom'
+
+export default function Category(){
+
+    const {apiEndpoint, defaultApiUrl} = useOutletContext()
+    
+    const [apiData, setApiData] = useState([])
+    const { slug } = useParams()
+
+    console.log("Denne kommer fra category", apiEndpoint)
+
+    const getSingleData = async()=>{
+        const response = await fetch(apiEndpoint ? apiEndpoint : defaultApiUrl+slug)
+        const data = await response.json()
+        setApiData(data)
+    
+    }
+
+    console.log("Cat", apiData, apiEndpoint)
+
+    useEffect(()=>{
+        getSingleData()
+    },[slug])
+
+    return (
+        <main>
+             <h1>{apiData?.name}</h1>
+             <section>
+                <h2>Bilder</h2>
+                <img src={apiData?.sprites?.front_default} alt={apiData?.name} />
+             </section>
+
+        </main>
+       
+    )
+}
---
+++ Routing/src/components/CategoryLayout.jsx
@@ -0,0 +1,46 @@
+import { Link, Outlet, UNSAFE_decodeViaTurboStream } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+export default function CategoryLayout(){
+    
+    const [apiData, setApiData] = useState([])
+    const [apiEndpoint, setApiEndpoint] = useState()
+
+    const defaultApiUrl = 'https://pokeapi.co/api/v2/'
+    
+    const getData = async()=>{
+        const response = await fetch(defaultApiUrl)
+        const data = await response.json()
+        const {type, pokemon, item} = data
+
+        setApiData({type, pokemon, item})
+        
+    }
+
+    console.log("Sjekk",apiData)
+    console.log("Denne kommer fra Layout",apiEndpoint) 
+
+    useEffect(()=>{
+        getData()
+    },[])
+    return (
+     <>
+        {/* Navigasjonsmeny for kategoriene */}
+        <nav className='main-nav'>
+            {Object.keys(apiData)?.map((item) => <Link key={item+'-saj'} to={item} onClick={()=>setApiEndpoint(defaultApiUrl + item )}>{item}</Link>)}
+            {/*apiData?.map((item) => <Link key={item.name+'-xt'} to={item.name} onClick={()=> setApiEndpoint(item.url)}>{item.name}</Link>)*/}
+        </nav>
+
+        {/* 
+          Outlet er plassen hvor child-routes rendres.
+          
+          Når vi er på:
+          /categories      -> <Categories /> vises her
+          /categories/sko  -> <Category /> vises her
+          
+          Det bestemmes av routene definert i App.jsx
+        */}
+        <Outlet context={{apiEndpoint, defaultApiUrl}} />
+    </>
+    )
+}
